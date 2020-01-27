23439244953610d4097ec5c6318d4926326bd49c
Max_Lebedev_P3211 <48407946+MaxLebedev2000@users.noreply.github.com>
2020-01-27 08:31:31 +0300

    Update OracleStorage.java

--- src/lab3/model/oracleDB/OracleStorage.java
+++ src/lab3/model/oracleDB/OracleStorage.java
@@ -31,7 +31,7 @@ public class OracleStorage extends JDBCDatabase implements Storage {
 
     @Override
     protected String password() {
-        return "*******";
+        return "Больше не прокатит )))";
     }
 
     @Override
